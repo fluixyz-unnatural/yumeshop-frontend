24aa6f27f7935e6c52133aa58bd559a874c0785a
fluixyz-unnatural <sudo3949@gmail.com>
2022-07-25 18:56:59 +0900

    feat: 値段表示の作成

--- src/components/atoms/Price/PriceDisplay.tsx
+++ src/components/atoms/Price/PriceDisplay.tsx
@@ -1,21 +1,41 @@
 import { VFC } from 'react';
 import { Price } from 'src/types/mock-api';
+import styled from 'styled-components';
 
 export type PriceProps = {
   price: Price;
 };
 
+const PriceSpan = styled.span<PriceProps>`
+  font-size: 24px;
+  margin: 0 4px;
+  color: ${(props) => (props.price.discounted ? 'red' : 'black')};
+`;
+const YenSpan = styled.span<PriceProps>`
+  color: ${(props) => (props.price.discounted ? 'red' : 'black')};
+`;
+const DisabledSpan = styled.span`
+  color: gray;
+  margin: 0 4px;
+`;
+
+const PriceWrapper = styled.div`
+  font-size: 14px;
+`;
+
 export const PriceDisplay: VFC<PriceProps> = ({ price }) => {
   if (price.discounted)
     return (
-      <div>
-        <span>税抜 {price.original_price}</span>
-        <span>{price.selling_price}円</span>
-      </div>
+      <PriceWrapper>
+        税抜
+        <DisabledSpan>{price.original_price}→</DisabledSpan>
+        <PriceSpan price={price}>{price.selling_price}</PriceSpan>
+        <YenSpan price={price}>円</YenSpan>
+      </PriceWrapper>
     );
   return (
-    <div>
-      <span>{price.selling_price}円</span>
-    </div>
+    <PriceWrapper>
+      <PriceSpan price={price}>{price.selling_price}</PriceSpan>円 円
+    </PriceWrapper>
   );
 };
