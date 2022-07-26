c7a4f6bf3d6cf50d886892ca0c5f4b47032c164a
fluixyz-unnatural <sudo3949@gmail.com>
2022-07-26 13:45:52 +0900

    fix: PC表示での間隔を広げる

--- src/components/organisms/Tags/Tags.tsx
+++ src/components/organisms/Tags/Tags.tsx
@@ -1,5 +1,6 @@
 import { VFC } from 'react';
 import { Tag, TagProps } from 'src/components/atoms/Tag';
+import { breakpoints } from 'src/styles/Tokens';
 import styled from 'styled-components';
 
 export type TagsProps = {
@@ -9,6 +10,9 @@ export type TagsProps = {
 const Wrapper = styled.div`
   display: flex;
   gap: 4px;
+  @media screen and (min-width: ${breakpoints.pc}) {
+    gap: 8px;
+  }
 `;
 
 export const Tags: VFC<TagsProps> = ({ tags }) => (
