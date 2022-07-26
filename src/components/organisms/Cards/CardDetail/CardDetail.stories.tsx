ee6a94821cacda9e4edd316b98b470a2cdf52af9
fluixyz-unnatural <sudo3949@gmail.com>
2022-07-26 11:27:34 +0900

    feat: storyを追加

--- src/components/organisms/Cards/CardDetail/CardDetail.stories.tsx
+++ src/components/organisms/Cards/CardDetail/CardDetail.stories.tsx
@@ -1,13 +1,14 @@
 import type { Meta, Story } from '@storybook/react/types-6-0';
-import { CardDetail } from './CardDetail';
+import { CardDetail, CardDetailProps } from './CardDetail';
 
 export default {
   component: CardDetail,
   title: 'organisms/CardDetail',
 } as Meta;
 
-const Template: Story = () => (
-  <CardDetail itemId="4dfd9672-5633-4328-b104-832e2f18c2a7" />
-);
+const Template: Story<CardDetailProps> = (args) => <CardDetail {...args} />;
 
-export const header = Template.bind({});
+export const Default = Template.bind({});
+Default.args = { itemId: 'e968f17c-2e63-4153-92f0-adfe52693e2e' };
+export const Discounted = Template.bind({});
+Discounted.args = { itemId: '4dfd9672-5633-4328-b104-832e2f18c2a7' };
